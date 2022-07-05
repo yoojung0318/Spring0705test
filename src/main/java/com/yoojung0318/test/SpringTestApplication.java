16584d96e5c4e2eacec84e4be06a23380c7c5a14
gyagoo <ish96116@gmail.com>
2022-07-05 20:24:30 +0900

    [김유정]  - 최초파일생성

---
+++ src/main/java/com/yoojung0318/test/SpringTestApplication.java
@@ -0,0 +1,16 @@
+package com.yoojung0318.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
+public class SpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yoojung0318/test/SpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoojung0318.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
